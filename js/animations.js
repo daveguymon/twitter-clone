d1e838a85a6c22873a7a62e521a121a8d9ce4aa4
Dave Guymon <davidwguymon@gmail.com>
2017-06-29 14:32:14 -0600

    Wrote code to decrement the remaining character count when user types status.

--- js/animations.js
+++ js/animations.js
@@ -1,38 +1,45 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-// hides tweet button and char count on page load
-  $('#tweet-controls').hide();
+    // hides tweet button and char count on page load
+    $('#tweet-controls').hide();
 
-// shows tweet button and char count when textarea is clicked
-// doubles size of textarea when clicked
-  $('textarea').click(function(){
-    $('#tweet-controls').show();
-    $(this).css('height','5em');
-  })
+    // shows tweet button and char count when textarea is clicked
+    // doubles size of textarea when clicked
+    $('textarea').on('click', function () {
+        $('#tweet-controls').show();
+        $(this).css('height', '5em');
+    })
+    
+    
+//    CONTROL CHARACTER COUNT WHEN USER TYPES STATUS
 
-// set #char-count to a variable, outside of a function
-var $maxCharacters = 140;
-$('#char-count').text($maxCharacters);
+    // set #char-count to a variable, outside of a function
+    var $maxChars = 140;
+//    set number of characters typed in textarea to a variable
+    var $typedChars = 0;
+    var $remainingChars = 140;
+    
+    $('textarea').on('keyup', function () {
+        $typedChars = $('textarea').val().length;
+        $remainingChars = $maxChars - $typedChars;
+       $('#char-count').text($remainingChars); 
+        if($remainingChars <= 10) {
+            $('#char-count').css('color', 'red');
+        }
+        else {
+           $('#char-count').css('color', 'grey'); 
+        }
+    })
+    
+//    END OF - CONTROL CHARACTER COUNT WHEN USER TYPES STATUS
+    
+        
+    
 
 
-// *******CONTINUE WORKING HERE*******
-// **************************************
-// decrement character count with each key press
-$('.tweet-compose').keypress(function(){
-  var $count = $('#char-count');
-  var $characters = $(this).val().length;
-
-  if($characters > $maxCharacters) {
-    $count.addClass('over');
-  } else {
-    $count.removeClass('over');
-  }
-  $count.text($maxCharacters - $count);
-})
-// **********CONTINUE WORKING ABOVE***********
 
 
 
 
-  // end of jquery script
+    // end of jquery script
 })
